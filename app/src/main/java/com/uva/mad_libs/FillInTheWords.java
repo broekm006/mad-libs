7e9a4a332482e21f50f31a2ddbb1529c36742767
Marc <marc-gw@live.nl>
2018-12-16 15:21:51 +0100

    updated design + cleaned code

--- app/src/main/java/com/uva/mad_libs/FillInTheWords.java
+++ app/src/main/java/com/uva/mad_libs/FillInTheWords.java
@@ -3,8 +3,6 @@ package com.uva.mad_libs;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.webkit.WebView;
-import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
